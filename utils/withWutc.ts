987596eefee4d8c0ae314eda8e567dd588b9f2cd
Artūras Chramčenko <arturasc@wix.com>
2019-08-07 16:31:34 +0300

    some typings

--- utils/withWutc.ts
+++ utils/withWutc.ts
@@ -8,14 +8,21 @@ export const WutcContext = React.createContext(null)
 /**
  * HOC to consume style props - consumed by WUTC user
  */
-export const withWutc = (children: any) => (props: any) => {
-  return React.createElement(WutcContext.Provider, {value: props}, React.createElement(children, props))
+export const withWutc = (children: React.FunctionComponent | React.ComponentClass | string) => (props: any) => {
+  return React.createElement(WutcContext.Provider, {
+    value: props,
+  }, React.createElement(children, props))
 }
 /**
  * HOC to pass style props to WUT wrapper only. Not used by WUTC consumer
  */
-export const getContext = (component: any) => React.forwardRef((props: any, ref: any) =>
-  React.createElement(WutcContext.Consumer, {}, (wutcContext: any) => React.createElement(component, {
-    ...props, ref, wutcContext,
-  })),
-)
+export const getContext = (component: React.FunctionComponent | React.ComponentClass | string) =>
+  React.forwardRef((props: any, ref: React.Ref<any>) =>
+    React.createElement(WutcContext.Consumer, {}, (wutcContext: any) =>
+      React.createElement(component, {
+        ...props,
+        ref,
+        wutcContext,
+      }),
+    ),
+  )
